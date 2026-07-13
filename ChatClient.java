e5d10a3b88132a93f3c787b1042116ab95428d32
Your Name <your-email@example.com>
2026-07-14 02:00:51 +0530

    LEVEL:0

---
+++ ChatClient.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.net.*;
+
+class ChatClient
+{
+    public static void main(String A[]) throws Exception
+    {
+        System.out.println("Client application is running...");
+
+        Socket sobj = new Socket("localhost",2100);
+    
+        System.out.println("Connection is succesful with server");
+   
+        PrintStream pobj = new PrintStream(sobj.getOutputStream());
+
+        BufferedReader bobj1 = new BufferedReader(new InputStreamReader(sobj.getInputStream()));
+
+        BufferedReader bobj2 = new BufferedReader(new InputStreamReader(System.in));
+        
+        System.out.println("-------------------------------------------");
+        System.out.println("--------- Marvellous Chat Client ----------");
+        System.out.println("-------------------------------------------");
+    
+        String str1 = null, str2 = null;
+
+        System.out.println("Enter message for server : ");
+
+        while(!(str1 = bobj2.readLine()).equals("end"))
+        {
+            pobj.println(str1);
+            str2 = bobj1.readLine();
+            System.out.println("Server says : "+str2);
+            System.out.println("Enter message for server : ");
+        }
+
+        sobj.close();
+    }
+}
---
+++ ChatServer.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.net.*;
+
+class ChatServer
+{
+    public static void main(String A[]) throws Exception
+    {
+        System.out.println("Server application is running...");
+
+        ServerSocket ssobj = new ServerSocket(2100);
+
+        System.out.println("Server is waiting at port 2100");
+
+        Socket sobj = ssobj.accept();
+
+        System.out.println("Client request gets accepted succesfully");
+
+        PrintStream pobj = new PrintStream(sobj.getOutputStream());
+
+        BufferedReader bobj1 = new BufferedReader(new InputStreamReader(sobj.getInputStream()));
+
+        BufferedReader bobj2 = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("-------------------------------------------");
+        System.out.println("--------- Marvellous Chat Server ----------");
+        System.out.println("-------------------------------------------");
+    
+        String str1 = null, str2 = null;
+
+        while((str1 = bobj1.readLine()) != null)
+        {
+            System.out.println("Client says : "+str1);
+            System.out.println("Enter message for client : ");
+            str2 = bobj2.readLine();
+            pobj.println(str2);
+        }
+
+        sobj.close();
+        ssobj.close();
+    }
+}
